f9ca93ca1939e914d8f7255f85b368c503301ec8
Michael Williams <dinosaur@riseup.net>
2015-02-12 01:00:09 +1300

    progress

--- jsondown.js
+++ jsondown.js
@@ -8,117 +8,113 @@ if (process.env.NODE_ENV !== 'production'){
 //  require('longjohn');
 }
 
-//var niceStringify = require('./nice-stringify');
-var niceStringify = require('json-stringify-safe');
+// use encodings from levelup
+var encodings = extend(require('levelup/lib/encodings'), {
+  // use json-stringify-safe instead of JSON.stringify
+  json: extend(encodings.json, {
+    encode: require('json-stringify-safe'),
+  }),
+});
 
 function noop() {}
 
-function FsDOWN(location, options) {
-  if (!(this instanceof FsDOWN))
-    return new FsDOWN(location, options);
-  MemDOWN.call(this, location);
-
-  options = options || {};
-
-  // use encodings from levelup
-  this.encodings = extend(encodings, {
-    // use nice-stringify as encoder instead of JSON.stringify
-    json: extend(encodings.json, {
-      encode: niceStringify,
-    }),
-  });
-
-  this.dataEncoding = 
-    typeof options.dataEncoding === 'string' ?
-      encodings[options.dataEncoding] :
-      options.dataEncoding || this.encodings.json;
+function fsDown(encoding) {
+  encoding = typeof encoding === 'string' ?
+      encodings[encoding] : encoding
   ;
-  this.fileEncoding = options.fileEncoding || "utf-8";
 
-  this._isLoadingFromFile = false;
-  this._isWriting = false;
-  this._queuedWrites = [];
+  function FsDOWN(location) {
+    if (!(this instanceof FsDOWN))
+      return new FsDOWN(location);
+    MemDOWN.call(this, location);
+
+    this._isLoadingFromFile = false;
+    this._isWriting = false;
+    this._queuedWrites = [];
+  }
+
+  util.inherits(FsDOWN, MemDOWN);
+
+  FsDOWN.prototype._jsonToBatchOps = function(data) {
+    return Object.keys(data).map(function(key) {
+      var value = data[key];
+      if (/^\$/.test(key)) {
+        key = key.slice(1);
+      } else {
+        try {
+          key = new Buffer(encoding.decode(key));
+        } catch (e) {
+          throw new Error('Error parsing key ' + encoding.encode(key) +
+                          ' as a buffer');
+        }
+      }
+      if (typeof(value) != 'string') {
+        try {
+          value = new Buffer(value);
+        } catch (e) {
+          throw new Error('Error parsing value ' + encoding.encode(value) +
+                          ' as a buffer');
+        }
+      }
+      return {type: 'put', key: key, value: value};
+    }, this);
+  };
+
+  FsDOWN.prototype._open = function(options, cb) {
+
+    fs.readFile(this.location, 'utf-8', function(err, data) {
+      if (err) {
+        if (err.code == 'ENOENT') return cb(null, this);
+        return cb(err);
+      }
+      try {
+        data = encoding.decode(data);
+      } catch (e) {
+        return cb(new Error('Error parsing JSON in ' + this.location +
+                            ': ' + e.message));
+      }
+      this._isLoadingFromFile = true;
+      try {
+        try {
+          this._batch(this._jsonToBatchOps(data), {}, noop);
+        } finally {
+          this._isLoadingFromFile = false;
+        }
+      } catch (e) {
+        return cb(e);
+      }
+      cb(null, this);
+    }.bind(this));
+  };
+
+  FsDOWN.prototype._writeToDisk = function(cb) {
+    if (this._isWriting)
+      return this._queuedWrites.push(cb);
+    this._isWriting = true;
+    fs.writeFile(this.location, encoding.encode(this._store), {
+      encoding: 'utf-8',
+    }, function(err) {
+      var queuedWrites = this._queuedWrites.splice(0);
+      this._isWriting = false;
+      if (queuedWrites.length)
+        this._writeToDisk(function(err) {
+          queuedWrites.forEach(function(cb) { cb(err); });
+        });
+      cb(err);
+    }.bind(this));
+  };
+
+  FsDOWN.prototype._put = function(key, value, options, cb) {
+    MemDOWN.prototype._put.call(this, key, value, options, noop);
+    if (!this._isLoadingFromFile) this._writeToDisk(cb);
+  };
+
+  FsDOWN.prototype._del = function(key, options, cb) {
+    MemDOWN.prototype._del.call(this, key, options, noop);
+    this._writeToDisk(cb);
+  };
+
+  return FsDOWN;
 }
 
-util.inherits(FsDOWN, MemDOWN);
-
-FsDOWN.prototype._jsonToBatchOps = function(data) {
-  return Object.keys(data).map(function(key) {
-    var value = data[key];
-    if (/^\$/.test(key)) {
-      key = key.slice(1);
-    } else {
-      try {
-        key = new Buffer(this.dataEncoding.decode(key));
-      } catch (e) {
-        throw new Error('Error parsing key ' + this.dataEncoding.encode(key) +
-                        ' as a buffer');
-      }
-    }
-    if (typeof(value) != 'string') {
-      try {
-        value = new Buffer(value);
-      } catch (e) {
-        throw new Error('Error parsing value ' + this.dataEncoding.encode(value) +
-                        ' as a buffer');
-      }
-    }
-    return {type: 'put', key: key, value: value};
-  }, this);
-};
-
-FsDOWN.prototype._open = function(options, cb) {
-
-  fs.readFile(this.location, this.fileEncoding, function(err, data) {
-    if (err) {
-      if (err.code == 'ENOENT') return cb(null, this);
-      return cb(err);
-    }
-    try {
-      data = this.dataEncoding.decode(data);
-    } catch (e) {
-      return cb(new Error('Error parsing JSON in ' + this.location +
-                          ': ' + e.message));
-    }
-    this._isLoadingFromFile = true;
-    try {
-      try {
-        this._batch(this._jsonToBatchOps(data), {}, noop);
-      } finally {
-        this._isLoadingFromFile = false;
-      }
-    } catch (e) {
-      return cb(e);
-    }
-    cb(null, this);
-  }.bind(this));
-};
-
-FsDOWN.prototype._writeToDisk = function(cb) {
-  if (this._isWriting)
-    return this._queuedWrites.push(cb);
-  this._isWriting = true;
-  fs.writeFile(this.location, this.dataEncoding.encode(this._store), {
-    encoding: this.fileEncoding,
-  }, function(err) {
-    var queuedWrites = this._queuedWrites.splice(0);
-    this._isWriting = false;
-    if (queuedWrites.length)
-      this._writeToDisk(function(err) {
-        queuedWrites.forEach(function(cb) { cb(err); });
-      });
-    cb(err);
-  }.bind(this));
-};
-
-FsDOWN.prototype._put = function(key, value, options, cb) {
-  MemDOWN.prototype._put.call(this, key, value, options, noop);
-  if (!this._isLoadingFromFile) this._writeToDisk(cb);
-};
-
-FsDOWN.prototype._del = function(key, options, cb) {
-  MemDOWN.prototype._del.call(this, key, options, noop);
-  this._writeToDisk(cb);
-};
-
-module.exports = FsDOWN;
+module.exports = fsDown;
--- test/test-yamldown.js
+++ test/test-yamldown.js
@@ -1,33 +1,39 @@
 var fs = require('fs');
 var should = require('should');
 var levelup = require('levelup');
+var yaml = require('js-yaml');
 var sinon = require('sinon');
 
-var JsonDOWN = require('../jsondown')('json');
+var YamlDOWN = require('../jsondown')({
+  encode: yaml.safeDump,
+  decode: yaml.safeLoad,
+  buffer: false,
+  type: 'yaml',
+});
 
-var LOCATION = '_testdb_JsonDOWN.json';
+var LOCATION = '_testdb_YamlDOWN.yaml';
 
 function removeLocation() {
   if (fs.existsSync(LOCATION)) fs.unlinkSync(LOCATION);
 }
 
 function getLocation() {
-  return JSON.parse(fs.readFileSync(LOCATION, 'utf-8'));
+  return yaml.safeLoad(fs.readFileSync(LOCATION, 'utf-8'));
 }
 
 function putLocation(obj) {
   if (typeof(obj) == 'object')
-    obj = JSON.stringify(obj);
+    obj = yaml.safeDump(obj);
   fs.writeFileSync(LOCATION, obj, 'utf-8');
 }
 
-describe('JsonDOWN', function() {
+describe('YamlDOWN', function() {
   beforeEach(removeLocation);
   afterEach(removeLocation);
 
   it('should raise error on corrupted data', function(done) {
-    putLocation('i am not valid json');
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    putLocation('i am not : - valid yaml');
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.open();
     db.on('error', function(err) {
       err.message.should.match(/^Error parsing JSON in _testdb/);
@@ -36,28 +42,28 @@ describe('JsonDOWN', function() {
   });
 
   it('should raise error on corrupted key', function(done) {
-    putLocation({'not_a_buffer': 'nope'});
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    putLocation({'not_a : - _buffer': 'nope'});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.open();
     db.on('error', function(err) {
-      err.message.should.eql('Error parsing key "not_a_buffer" as a buffer');
+      err.message.should.eql('Error parsing key "not_a : - _buffer"\n as a buffer');
       done();      
     });
   });
 
   it('should raise error on corrupted value', function(done) {
     putLocation({'$hi': false});
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.open();
     db.on('error', function(err) {
-      err.message.should.eql('Error parsing value false as a buffer');
+      err.message.should.eql('Error parsing value false\n as a buffer');
       done();      
     });
   });
 
   it('should get existing keys', function(done) {
     putLocation({'$hey': 'there'});
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.get('hey', function(err, value) {
       if (err) return done(err);
       value.should.eql('there');
@@ -66,7 +72,7 @@ describe('JsonDOWN', function() {
   });
 
   it('should raise error on nonexistent keys', function(done) {
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.get('nonexistent', function(err, value) {
       err.notFound.should.be.true;
       done();
@@ -75,7 +81,7 @@ describe('JsonDOWN', function() {
 
   it('should support binary keys', function(done) {
     putLocation({'[1,2,3]': 'yo'});
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.get(new Buffer([1,2,3]), function(err, value) {
       if (err) return done(err);
       value.should.eql('yo');
@@ -85,7 +91,7 @@ describe('JsonDOWN', function() {
 
   it('should support binary values', function(done) {
     putLocation({'$hello': [1,2,3]});
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.get('hello', function(err, value) {
       if (err) return done(err);
       value.should.eql(new Buffer([1,2,3]));
@@ -95,7 +101,7 @@ describe('JsonDOWN', function() {
 
   it('should delete', function(done) {
     putLocation({'$whats': 'up'});
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.del('whats', function(err) {
       if (err) return done(err);
       getLocation().should.eql({});
@@ -104,7 +110,7 @@ describe('JsonDOWN', function() {
   });
 
   it('should put', function(done) {
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     db.put('foo', 'bar', function(err) {
       if (err) return done(err);
       getLocation().should.eql({$foo: 'bar'});
@@ -113,7 +119,7 @@ describe('JsonDOWN', function() {
   });
 
   it('should intelligently queue writes', function(done) {
-    var db = levelup(LOCATION, {db: JsonDOWN});
+    var db = levelup(LOCATION, {db: YamlDOWN});
     sinon.spy(fs, 'writeFile');
     db.put('foo', 'bar');
     db.put('lol', 'cats');
